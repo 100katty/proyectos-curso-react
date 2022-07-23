9df3a3ed00753771071d3f12210df49c31d1ea0f
100katty <ana.hernandez@hackademy.mx>
2022-07-22 22:38:01 -0500

    toEquals and toStringEqual

--- 03-counter-app/tests/demo.test.js
+++ 03-counter-app/tests/demo.test.js
@@ -1,10 +1,12 @@
 
-test('Esta prueba no debe de falla',() => {
-    
-    //1.Inicializacion
-    const message1 = 'Hola Mundo'
-    //2.Estimulo
-    const message2 = message1.trim();
-    //3.Observar el compartamiento
-    expect(message1).toBe(message2);
-})
+describe('Pruebas en <DemoComponent />', () => {
+    test('Esta prueba no debe de falla', () => {
+        
+        //1.Inicializacion
+        const message1 = 'Hola Mundo'
+        //2.Estimulo
+        const message2 = message1.trim();
+        //3.Observar el compartamiento
+        expect(message1).toBe(message2);
+    });
+});
