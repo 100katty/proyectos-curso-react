7deed110af90521ee597b0bca8c87f9afe15e15b
100katty <ana.hernandez@hackademy.mx>
2022-07-22 21:26:41 -0500

    use tobe

--- 03-counter-app/tests/demo.test.js
+++ 03-counter-app/tests/demo.test.js
@@ -1,7 +1,10 @@
 
 test('Esta prueba no debe de falla',() => {
     
-    if (1 === 0) {
-        throw new Error('No puede dividir entre cero');
-    }
+    //1.Inicializacion
+    const message1 = 'Hola Mundo'
+    //2.Estimulo
+    const message2 = message1.trim();
+    //3.Observar el compartamiento
+    expect(message1).toBe(message2);
 })
