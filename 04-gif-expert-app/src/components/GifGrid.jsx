460555abfe9c4aa9f3a3c065aa687f29803e46cc
100katty <ana.hernandez@hackademy.mx>
2022-08-01 18:52:51 -0500

    proyect giftexpert

---
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -0,0 +1,25 @@
+import React , {useEffect} from 'react'
+import { getGifs } from '../helpers/getGifs';
+
+export const GifGrid = ({ category }) => {
+
+
+   
+    
+    useEffect(() => {
+       getGifs(category);
+    }, [])
+    
+    
+
+
+  return (
+      <>
+          <h3>{category}</h3>
+          <ol>
+              <li></li>
+          </ol>
+         
+      </>
+  )
+}
